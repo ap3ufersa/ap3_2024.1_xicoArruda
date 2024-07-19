d5f92363a76392b0f2103fa0d48883feb102062b
Xico <ap3ufersa@gmail.com>
2024-07-19 19:40:44 -0300

    Atualizsei o aaplicativo

--- unidade1/gabarito_aula1/ap3_xico/src/App.java
+++ unidade1/gabarito_aula1/ap3_xico/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Ola, mundo!");
+        System.out.println("Ola, mundo de novo!");
     }
 }
