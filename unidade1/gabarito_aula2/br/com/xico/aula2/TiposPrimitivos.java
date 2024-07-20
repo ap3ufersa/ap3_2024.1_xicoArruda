68134ce2aaea8a4b8e8de215a9b47f2eb173fc2a
Xico <ap3ufersa@gmail.com>
2024-07-19 22:00:13 -0300

    Create TiposPrimitivos.java

---
+++ unidade1/gabarito_aula2/br/com/xico/aula2/TiposPrimitivos.java
@@ -0,0 +1,21 @@
+//fully qualified name
+package br.com.xico.aula2;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+
+        byte umByte = 112;
+        short umShort = 10;
+        int umInt = 1235;
+        Integer umInteger = 1235;
+
+        System.out.println("");
+        System.out.println("Imprimindo tipos:");
+        System.out.println("umByte=" + umByte);
+        System.out.println("umShort=" + umShort);
+        System.out.println("umInt=" + umInt);
+        System.out.println("umInteger=" + umInteger);
+
+    }
+
+}
