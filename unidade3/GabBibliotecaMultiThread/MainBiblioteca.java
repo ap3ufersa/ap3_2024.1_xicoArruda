3fa7b057c35188ca89f78717fac3c40d75fd935a
Xico <ap3ufersa@gmail.com>
2024-10-16 10:07:06 -0300

    Ajustando threads.

--- unidade3/GabBibliotecaMultiThread/MainBiblioteca.java
+++ unidade3/GabBibliotecaMultiThread/MainBiblioteca.java
@@ -6,12 +6,14 @@ public class MainBiblioteca {
         // Criar e executar threads de adição
         for (int i = 1; i <= quantOperacoes; i++) {
             Livro livro = new Livro("Livro " + i, "Autor " + i);
+            System.out.println("Adicionando livro:" + livro);
             new AdicionarLivroThread(biblioteca, livro).start();
         }
 
         // Criar e executar threads de remoção
         for (int i = 1; i <= quantOperacoes; i++) {
             String titulo = "Livro " + i;
+            System.out.println("Removendo livro:" + titulo);
             new RemoverLivroThread(biblioteca, titulo).start();
         }
 
---
+++ unidade3/zFiosDeExecucao.java
@@ -0,0 +1,22 @@
+public class zFiosDeExecucao {
+
+  public static void main(String[] args) {
+    // Thread atual
+    Thread fioMain = Thread.currentThread();
+    String nome = fioMain.getName();
+    System.out.println(nome);
+
+    MeuRunnable meuRunnable = new MeuRunnable();
+
+    Thread fio0 = new Thread(meuRunnable);
+    Thread fio1 = new Thread(() ->
+    System.out.println(Thread.currentThread().getName()));
+    Thread fio2 = new Thread(meuRunnable);
+    Thread fio3 = new Thread(meuRunnable);
+
+    fio0.start();
+    fio1.start();
+    fio2.start();
+    fio3.start();
+  }
+}
